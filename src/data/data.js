07941a0b62b3b42a20236c7a9cec7e6b033e34d4
Bond <hydrogenbond68@gmail.com>
2024-05-01 09:59:44 +0300

    first commit

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Kibee";
+const city = "United Kingdom";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
